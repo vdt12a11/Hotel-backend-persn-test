4b47d15a14d03f66020290379bb03ce6b0cd23c9
vdt12a11 <ductuan28122005@gmail.com>
2025-12-18 23:14:14 +0700

    Remove employee functions from roomController
    Removed employee-related functions from roomController.

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -34,41 +34,7 @@ const getRoom = async (req, res) => {
     }
     res.json(room);
 }
-const updateEmployee = async (req, res) => {
-    if (!req?.body?.id) {
-        return res.status(400).json({ 'message': 'ID parameter is required.' });
-    }
 
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
-    }
-    if (req.body?.firstname) employee.firstname = req.body.firstname;
-    if (req.body?.lastname) employee.lastname = req.body.lastname;
-    const result = await employee.save();
-    res.json(result);
-}
-
-const deleteEmployee = async (req, res) => {
-    if (!req?.body?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
-
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
-    }
-    const result = await employee.deleteOne(); //{ _id: req.body.id }
-    res.json(result);
-}
-
-const getEmployee = async (req, res) => {
-    if (!req?.params?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
-
-    const employee = await Employee.findOne({ _id: req.params.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.params.id}.` });
-    }
-    res.json(employee);
-}
 
 module.exports = {
     getAllRoom,
